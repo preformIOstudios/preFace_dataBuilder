312a3c4bdfff28b3e3d590c412b0c2766af0a21d
david allen <mail@ludimotion.com>
2015-09-27 05:20:35 -0400

    initial JSON includes

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxJSON.h"
 
 class ofApp : public ofBaseApp{
 
@@ -18,5 +19,7 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+        ofxJSONElement result;
 		
 };
