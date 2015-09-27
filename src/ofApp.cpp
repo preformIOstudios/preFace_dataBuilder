096724a7194d7e8be4680b697e54cf535e89c43e
david allen <mail@ludimotion.com>
2015-09-27 05:56:52 -0400

    ofxFaceTracker declarations

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,8 +1,45 @@
 #include "ofApp.h"
 
+ofxFaceTracker::Gesture gestureIds[] = {
+    ofxFaceTracker::MOUTH_WIDTH,
+    ofxFaceTracker::MOUTH_HEIGHT,
+    ofxFaceTracker::LEFT_EYEBROW_HEIGHT,
+    ofxFaceTracker::RIGHT_EYEBROW_HEIGHT,
+    ofxFaceTracker::LEFT_EYE_OPENNESS,
+    ofxFaceTracker::RIGHT_EYE_OPENNESS,
+    ofxFaceTracker::JAW_OPENNESS,
+    ofxFaceTracker::NOSTRIL_FLARE
+};
+
+string gestureNames[] = {
+    "mouthWidth",
+    "mouthHeight",
+    "leftEyebrowHeight",
+    "rightEyebrowHeight",
+    "leftEyeOpenness",
+    "rightEyeOpenness",
+    "jawOpenness",
+    "nostrilFlare"
+};
+
+int gestureCount = 8;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     cout << "Hello World!" << endl;
+    
+    imageNamePrefix = "video_";
+    string videoFileName = imageNamePrefix + "480";
+    video.loadMovie( videoFileName + ".mov");
+    tracker.setup();
+    tracker.setRescale(.25);
+    tracker.setIterations(100);
+    tracker.setClamp(10);
+    tracker.setTolerance(.5);
+    tracker.setAttempts(4);
+    imageIndex = 0;
+    imageSnapshot = false;
+    img.allocate(video.getWidth(), video.getHeight(), OF_IMAGE_COLOR);
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxCv.h"
+#include "ofxFaceTracker.h"
 #include "ofxJSON.h"
+
+using namespace ofxCv;
 
 class ofApp : public ofBaseApp{
 
@@ -20,6 +24,14 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
     
+        ofVideoPlayer video;
+        ofxFaceTracker tracker;
+        vector<ofMesh> trackedFrames;
+        vector<vector<float> > trackedGestures;
         ofxJSONElement result;
+        int imageIndex;
+        bool imageSnapshot;
+        ofImage img;
+        string imageNamePrefix;
 		
 };
